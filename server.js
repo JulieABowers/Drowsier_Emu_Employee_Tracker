3914f60e50d33f3b7bba29018e3470f086000ae7
JulieABowers <Haldirolorien@comcast.net>
2024-01-31 19:30:24 -0600

    Set db connection up

---
+++ server.js
@@ -0,0 +1,24 @@
+// Import required
+const express = require('express');
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection({
+        host: 'localhost',
+        // MySQL Username
+        user: 'root',
+        password: 'root',
+        database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
+
